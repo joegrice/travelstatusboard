9478d3e819c7c63f07e4b3df953e3981818caa62
Joe Grice <joeisnotamused@gmail.com>
2019-06-05 14:11:45 +0100

    Added some file structure and basic component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import './style';
 import { Component } from 'preact';
+import BusPrediction from './Components/BusPredict/BusPredict';
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello, World!</h1>
+				<h1><BusPrediction /></h1>
 			</div>
 		);
 	}
